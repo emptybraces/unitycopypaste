2b462985ca45471a4d80cf3dc20476f3af6a25c5
tomop <maximilianahead@gmail.com>
2019-12-14 23:40:32 +0900

    プロジェクト作成

---
+++ Assets/RandomRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+	public class RandomRotate : MonoBehaviour
+	{
+		[SerializeField] Vector3 spd;
+		[SerializeField] Vector3 limit;
+
+		void Update()
+		{
+			var t = Time.time * spd;
+			transform.eulerAngles = new Vector3(
+				(Mathf.PerlinNoise(t.x + 0, t.x + 0) - 0.5f) * limit.x,
+				(Mathf.PerlinNoise(t.y + 1, t.y + 1) - 0.5f) * limit.y,
+				(Mathf.PerlinNoise(t.z + 2, t.z + 2) - 0.5f) * limit.z);
+		}
+	}
+}
